533ce05ab2d219ea8aa4fc2b2e93ad7ffb75dd47
Anjali Choudhary <anjalichoudhary1202@gmail.com>
2024-01-27 02:13:13 +0530

    Added avatars functionality and SetAvatar Component/page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
-import React,{useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import React,{useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 import {ToastContainer, toast} from "react-toastify";
@@ -8,6 +8,7 @@ import axios from "axios";
 import { registerRoute } from '../utils/APIRoutes';
 
 function Register() {
+  const navigate = useNavigate();
   const[values, setValues] = useState({
     username: "",
     email: "",
@@ -21,17 +22,30 @@ function Register() {
     pauseOnHover: true,
     draggable: true,
     theme: "dark",
-  }
+  };
+
+  useEffect(()=> {
+    if(localStorage.getItem('chat-app-user')) {
+      navigate("/");
+    }
+  }, []);
   const handleSubmit = async (event) => {
     event.preventDefault();
     if(handleValidation()) {
-      console.log("in validation", registerRoute);
-      const {password, confirmPassword, username, email} = values;
+      const {password, username, email} = values;
       const {data} = await axios.post(registerRoute, {
         username,
         email,
         password,
       });
+      if(data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+
+      if( data.status === true) {
+        localStorage.setItem('chat-app-user', JSON.stringify(data.user));
+        navigate("/");
+      }
     }
   };
 
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -22,7 +22,44 @@ module.exports.register = async (req,res,next) => {
         return res.json({status: true, user});
     }
     catch(err) {
-        
+        next(err);
     }
 };
 
+module.exports.login = async (req,res,next) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+
+        if(!user) {
+            return res.json({msg : "Incorrect username or password", status: false});
+        }
+        
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.json({msg : "Incorrect username or password", status: false});
+        }
+        delete user.password;
+        
+        return res.json({status: true, user});
+    }
+    catch(err) {
+        next(err);
+    }
+};
+    
+module.exports.setAvatar = async (req,res,next) => {
+    try{
+        const userId = req.params.id;
+        const avatarImage = req.body.image;
+        const userData = await User.findByIdAndUpdate(userId, {
+            isAvatarImageSet: true,
+            avatarImage,
+        });
+
+        return res.json({isSet:userData.isAvatarImageSet, image:userData.avatarImage});
+        
+    } catch(err) {
+        next(err);
+    }
+};
